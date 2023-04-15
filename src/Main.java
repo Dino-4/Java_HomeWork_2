109d98b13902d05aa4b33c40e1783b82f412b333
DiNo-4 <120940819+Dino-4@users.noreply.github.com>
2023-04-15 23:39:48 +0400

    origin

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int x
+        int ticket_price = 5000;
+        int price_of_one_mile = 20;
+        System.out.println("Начисленно миль: " + ticket_price/price_of_one_mile);
     }
 
 }
